57e007889b0fdc7c66949bc6a1f9e06c28233148
Jeffrey Rennie <rennie@google.com>
2016-09-28 16:39:13 -0700

    Bookshelf lives again!

--- bookshelf/Startup.cs
+++ bookshelf/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoogleCloudSamples.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,13 +8,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Microsoft.Data.Entity;
 
 namespace GoogleCloudSamples
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        private ILoggerFactory _loggerFactory;
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            _loggerFactory = loggerFactory;
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -27,6 +33,28 @@ namespace GoogleCloudSamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var logger = _loggerFactory.CreateLogger("ConfigureServices");
+            services.AddAntiforgery();
+            // Choose a a backend to store the books.
+            switch (Configuration["Data:BookStore"]?.ToLower())
+            {
+                case "sqlserver":
+                    AddSqlServer(services);
+                    logger.LogInformation("Storing book data in SQL Server.");
+                    break;
+
+                case "datastore":
+                    AddDatastore(services);
+                    logger.LogInformation("Storing book data in Datastore.");
+                    break;
+
+                default:
+                    Halt("No bookstore backend selected.\n" +
+                        "Set the configuration variable Data:BookStore to " +
+                        "one of the following: sqlserver, postgres, datastore.");
+                    break;
+            }
+
             // Add framework services.
             services.AddMvc();
         }
@@ -59,6 +87,37 @@ namespace GoogleCloudSamples
                     template: "_ah/health",
                     defaults: new { controller = "Home", action = "Health" });
             });
+        }
+        public void Halt(string message)
+        {
+            var logger = _loggerFactory.CreateLogger("Halt");
+            logger.LogCritical(message);
+            if (Debugger.IsAttached)
+                Debugger.Break();
+            Environment.Exit(-1);
+        }
+
+        private void AddDatastore(IServiceCollection services)
+        {
+            string projectId = Configuration["GOOGLE_PROJECT_ID"];
+            if (string.IsNullOrWhiteSpace(projectId))
+                Halt("Set the configuration variable GOOGLE_PROJECT_ID.");
+            services.Add(new ServiceDescriptor(typeof(IBookStore),
+                (x) => new DatastoreBookStore(projectId),
+                ServiceLifetime.Singleton));
+        }
+
+        private void AddSqlServer(IServiceCollection services)
+        {
+            var entityFramework = services.AddEntityFramework();
+            string sqlserverConnectionString =
+                Configuration["Data:SqlServer:ConnectionString"];
+            if (string.IsNullOrWhiteSpace(sqlserverConnectionString))
+                Halt("Set the configuration variable Data:SqlServer:ConnectionString.");
+            entityFramework.AddSqlServer()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(sqlserverConnectionString));
+            services.AddScoped(typeof(IBookStore), typeof(DbBookStore));
         }
     }
 }
